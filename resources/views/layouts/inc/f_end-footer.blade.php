050c52f1ea8d9f8f4e3768b71d93aa971e7794a1
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2022-12-22 13:14:41 +0100

    resources/views/layouts/inc/f_end-footer.blade.php file

---
+++ resources/views/layouts/inc/f_end-footer.blade.php
@@ -0,0 +1,11 @@
+<footer
+class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-[#881337] text-white h-24 mt-24 opacity-90 md:justify-center"
+>
+{{-- <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p> --}}
+
+<a
+    href="{{url('admin/add-post')}}"
+    class="absolute top-1/3 right-32 bg-black text-white py-2 px-5 "
+    >Post Article</a
+>
+</footer>
