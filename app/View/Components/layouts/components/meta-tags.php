6020d70cd8e72db58d98eb4baa5d8c22c4c2ce12
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2022-12-22 13:22:51 +0100

    app/View/

---
+++ app/View/Components/layouts/components/meta-tags.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components\layouts\components;
+
+use Illuminate\View\Component;
+
+class meta-tags extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.layouts.components.meta-tags');
+    }
+}
